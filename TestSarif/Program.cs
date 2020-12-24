1f77d632e93a62c739f6fd9062712c4b7035fabf
IchNikola <55153245+IchNikola@users.noreply.github.com>
2020-12-24 15:32:00 +0300

    Add files via upload

---
+++ TestSarif/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestSarif
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = Formula42(3, 5);
+        }
+
+        static int Formula42(int? coefficientA, int? coefficientB)
+        {
+            var data = new List<int>();
+            if (coefficientA != null)
+                data.Add(Formula42(coefficientA.Value));
+            else if (coefficientB != null)
+                data.Add(Formula42(coefficientA.Value));
+            return data.SingleOrDefault();
+        }
+
+        static private int Formula42(int coefficient)
+        {
+            return coefficient;
+        }
+    }
+}
